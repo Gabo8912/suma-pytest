2d4b466701e12d3088d86dec3fe11c77665204d7
Gabo8912 <128503197+Gabo8912@users.noreply.github.com>
2025-06-06 09:21:13 -0600

    Arreglo de ylm

--- tests/test_suma.py
+++ tests/test_suma.py
@@ -1,8 +1,8 @@
-# archivo: test_suma.py
-from suma import sumar
-
+# archivo: test_ejemplo.py
+def sumar(a, b):
+    return a + b
 def test_sumar_correcto():
     assert sumar(2, 3) == 5
-
+# Esto fallará
 def test_sumar_falla():
-    assert sumar(2, 2) == 5  # Esto fallará a propósito
+    assert sumar(2, 2) == 5
